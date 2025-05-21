a57613eb34eb830a1e90d0768d72567e48a4e5a0
Modestas Vaitkevičius <info@modestasv.lt>
2025-05-21 15:10:32 +0300

    Small issue with validation not showing up

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -18,6 +18,6 @@
 <input type="{{ $type }}" id="{{ $name }}" placeholder="{{ $placeholder }}" name="{{ $name }}"
     {{ $attributes->merge(['class' => 'w-full px-4 py-2 rounded-lg text-gray-700 dark:text-gray-300 bg-gray-50 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent']) }}>
 
-@error('email')
+@error($name)
     <span class="text-red-500">{{ $message }}</span>
 @enderror
