3e6f56aa09e8e1f59fb20ee3237ebd983bb2353d
Modestas Vaitkevičius <info@modestasv.lt>
2025-05-21 15:17:14 +0300

    Updated checkbox component to accept value

--- resources/views/components/forms/checkbox.blade.php
+++ resources/views/components/forms/checkbox.blade.php
@@ -1,7 +1,8 @@
-@props(['label', 'name'])
+@props(['label', 'name', 'value' => null])
+
 <label for="{{ $name }}"
     {{ $attributes->merge(['class' => 'ml-2 block text-sm text-gray-700 dark:text-gray-300']) }}>
-    <input type="checkbox" id="{{ $name }}" name="{{ $name }}"
-        class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded">
+    <input type="checkbox" id="{{ $name }}" name="{{ $name }}" value="{{ $value }}"
+        {{ $attributes }} class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded">
     {{ $label }}
 </label>
